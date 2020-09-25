6b4414e68910c508e4eaa2734ef573b2f3baeeae
Jerry Doran <Jerry.Doran@pccairfoils.com>
2020-09-25 13:26:54 -0400

    finished project

---
+++ main.js
@@ -0,0 +1,20 @@
+const menuBtn = document.querySelector('.menu-btn');
+const menuItems = document.querySelector('.menu-items');
+const menuItem = [...document.querySelectorAll('.menu-item')];
+
+const toggle = () => {
+  menuBtn.classList.toggle('open');
+  menuItems.classList.toggle('open');
+};
+
+menuBtn.addEventListener('click', () => {
+  toggle();
+});
+
+menuItem.forEach((item) => {
+  item.addEventListener('click', () => {
+    if (menuBtn.classList.contains('open')) {
+      toggle();
+    }
+  });
+});
